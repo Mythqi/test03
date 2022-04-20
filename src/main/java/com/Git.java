2b87e87810ff9ebbd4cb00a0d39d2b1fcc0dbdbd
guyiqi <2201388930@qq.com>
2022-04-20 21:19:21 +0800

    第一次提交

---
+++ src/main/java/com/Git.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * Description:
+ *
+ * @Autor: Rolin
+ * datetime: 04-20-21:13
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("1st to commit");
+    }
+}
